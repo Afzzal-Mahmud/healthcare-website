7b6d3b49949558cf4b9ad870a60794596b3f21cc
afzal mahmud <afzzalmahmud@gmail.com>
2021-10-19 17:27:23 +0600

    add conditional rendaring on header , init the firebase for deploy

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./Header.css"
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import useAuth from "../../Hooks/useAuth";
 
 function Header() {
+    const {user,logOut} = useAuth()
     return(
         <Navbar collapseOnSelect expand="lg" className='nav-bar sticky-top'>
   <Container className='padding'>
@@ -11,17 +13,33 @@ function Header() {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="ms-auto fw-bold">
-      <Nav.Link as={Link} to="home">Home</Nav.Link>
-      <Nav.Link as={Link} to="home">Services</Nav.Link>
-      <Nav.Link as={Link} to="doctors">Doctors</Nav.Link>
-      <Nav.Link as={Link} to="products">Products</Nav.Link>
+      <Nav.Link as={Link} to="/home">Home</Nav.Link>
+      <Nav.Link as={Link} to="services">Services</Nav.Link>
+      <Nav.Link as={Link} to="/doctors">Doctors</Nav.Link>
+      <Nav.Link as={Link} to="/faq">F.A.Q</Nav.Link>
+
+      {/* showing the user name on header if he is log In */}
+      {user.email && <Nav.Link>{user?.displayName}</Nav.Link>}
 
     </Nav>
     <Nav>
       <Nav.Link eventKey={2}>
-      <Nav.Link as={Link} to="login">
+        {/* render conditionali if user have already logIn display LogOut btn or See the logIn button and on clicking the log In button it will take back to the home components */}
+      {
+        user.email ? 
+        <Nav.Link as={Link} to='/home'>  
+        <Button 
+        onClick={logOut}
+        className='primary-background' 
+        >Log Out</Button>
+        </Nav.Link>
+
+        : 
+
+        <Nav.Link as={Link} to="/login">
         <Button className='primary-background'>Log In</Button>
-      </Nav.Link>
+        </Nav.Link>
+      }
       </Nav.Link>
     </Nav>
   </Navbar.Collapse>
